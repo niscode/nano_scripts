9d6cdf6a6579cc57788113d9bc14d5d47a4e2dde
niscode <nishimura.s405@gmail.com>
2023-01-27 13:58:20 +0900

    chimeSwitch updated

--- run_chimeSwitch.sh
+++ run_chimeSwitch.sh
@@ -10,30 +10,32 @@ do
   sleep 10
   count=`ps -ef | grep sudo | grep -v grep | wc -l`
   # sudoプロセスを監視
+
   if [ $count = 0 ]; then
     echo "[-]chimeSwitch Process Down  -----"
     echo "[+]chimeSwitch Process Start +++++"
-    echo 'capf' | sudo -S python3 ~/nano_scripts/keyboard_ChimeSwitch.py CA000 ignis2-sock.ca-platform.org 11001 CA010 >> chime.log
+    echo 'capf' | sudo -S python3 ~/nano_scripts/keyboard_ChimeSwitch.py CA000 ignis2-sock.ca-platform.org 11001 CA010
+    # echo 'capf' | sudo -S python3 ~/nano_scripts/keyboard_ChimeSwitch.py CA000 ignis2-sock.ca-platform.org 11001 CA010 >> chime.log
 
     # keyboard_ChimeSwitch.pyの実行が何らかのエラーによって止まったとき
-    echo $((exp ++))
-
-  else
-    # echo "[+]chimeSwitch Process OK    +++++"
-    # sleep 10
-    if [ $exp = 2 ] || [ $((exp / 2)) = 0 ]; then
-      # 初回起動の停止時と20秒に1回、プロセスを強制キル
-      pid=`ps -ax | grep sudo | grep keyboard | awk '{ print $1 }'`
-      echo 'capf' | sudo -S kill -9 $pid
-    else
-      sleep 10
-    fi
-    echo "[-]chimeSwitch Process Quited    -----"
-    echo $((exp ++))
-  fi
+    # echo $((exp ++))
 
   # else
-  #   echo "[+]chimeSwitch Process OK    +++++"
-  #   sleep 10
+  #   # echo "[+]chimeSwitch Process OK    +++++"
+  #   # sleep 10
+  #   if [ $exp = 2 ] || [ $((exp / 2)) = 0 ]; then
+  #     # 初回起動の停止時と20秒に1回、プロセスを強制キル
+  #     pid=`ps -ax | grep sudo | grep keyboard | awk '{ print $1 }'`
+  #     echo 'capf' | sudo -S kill -9 $pid
+  #   else
+  #     sleep 10
+  #   fi
+  #   echo "[-]chimeSwitch Process Quited    -----"
+  #   # echo $((exp ++))
   # fi
+
+  else
+    echo "[-]chimeSwitch Process Quited    -----"
+    sleep 10
+  fi
 done
